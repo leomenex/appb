0f483ff4a34ad2d9cb8164d6f09bd715ff6d3b06
Danilo Veloso <danilovsdanilo@gmail.com>
2024-05-22 16:51:14 -0400

    definicao de layout

---
+++ app/src/components/BaseSection.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row justify-between">
+    <div>
+      <h5 class="text-weight-bold q-ma-none">{{ title }}</h5>
+      <div class="circle-full"></div>
+    </div>
+    <q-btn flat color="text-more" label="Ver Todos" />
+    <!-- <div class="text-more">
+      Ver Todos
+    </div> -->
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'base-section'
+})
+
+const props = defineProps({
+  title: String,
+})
+</script>
+
+<style>
+.circle-full {
+  height: 15px;
+  width: 15px;
+  border-radius: 10px;
+  background-color: #389ed0;
+}
+
+.text-more {
+  color: #c5cfe0;
+}
+
+.q-btn__content {
+  text-transform: capitalize !important;
+}
+</style>
---
+++ app/src/layouts/MainFooter.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue';
+
+const tab = ref(0);
+const linksList = [
+  {
+    name: 'home',
+    label: 'Início',
+    icon: 'home',
+    link: 'https://quasar.dev'
+  },
+  {
+    name: 'service',
+    label: 'Serviços',
+    icon: 'support_agent',
+    link: 'https://quasar.dev'
+  },
+  {
+    name: 'courses',
+    label: 'Cursos',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    name: 'profile',
+    label: 'Perfil',
+    icon: 'person',
+    link: 'https://quasar.dev'
+  },
+]
+
+</script>
+
+<template>
+  <q-footer bordered class="bg-blue ">
+    <div style="height: 10px;"></div>
+
+    <q-tabs v-model="tab" dense style="border-radius: 20px 20px 0 0;" class="bg-white">
+      <q-tab v-for="(item, index) in linksList" :key="index" :name="item.name" :icon="item.icon" :label="item.label" />
+    </q-tabs>
+  </q-footer>
+</template>
+
+<style>
+.q-tab--inactive {
+  color: #c5cfe0;
+}
+
+.q-tab--active {
+  color: #389ed0;
+}
+
+.q-tab__label {
+  font-size: 15px;
+  text-transform: capitalize;
+}
+</style>
